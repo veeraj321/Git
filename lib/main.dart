c8889bf7494bf0d9afa9f64bf749bf4dddb0f1bd
Divya Shree <divya.shree@exathought.com>
2023-07-13 01:18:23 +0530

    scaffoldkey create madu

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,17 @@ class ScrumPoker extends StatelessWidget {
   final AppRouterDelegate _appRouterDelegate = AppRouterDelegate();
   final AppRouteInformationParser _appRouteInformationParser =
       AppRouteInformationParser();
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routeInformationParser: _appRouteInformationParser,
-      routerDelegate: _appRouterDelegate,
-      title: 'Scrum Poker',
-      theme: appLightTheme,
-      debugShowCheckedModeBanner: false,
-    );
+        routeInformationParser: _appRouteInformationParser,
+        routerDelegate: _appRouterDelegate,
+        title: 'Scrum Poker',
+        theme: appLightTheme,
+        debugShowCheckedModeBanner: false);
   }
 }
